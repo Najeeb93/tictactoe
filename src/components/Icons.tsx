6b7c3922bfcb67c12088c9469c365fdeabb0602d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-14 16:49:07 +0500

    add to commit

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import type { PropsWithChildren } from "react";
+import Icon from 'react-native-vector-icons/FontAwesome'
 
+type IconsProps = PropsWithChildren<{
+    name: string;
 
-export default function Icons() {
-    return (
-        <div>Icons</div>
-    )
+}>
+
+export default function Icons({name}) {
+    switch (name) {
+        case 'circle':
+            return <Icon name="circle-then" />
+            break;
+    
+        default:
+            break;
+    }
 }
