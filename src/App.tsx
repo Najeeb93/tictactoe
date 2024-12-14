8623acad18f20928d3782a89e067e1f4126a6e51
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-12-14 17:28:36 +0500

    add to commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ export default function App(): JSX.Element {
   const [gameWinner, setGameWinner] = useState<string>('');
   const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
 
+  const reloadGame = () => {
+    setIsCross(false)
+    setGameWinner('')
+    setGameState(new Array(9).fill('empty', 0, 9))
+  }
   return (
     <SafeAreaView>
       <StatusBar />
